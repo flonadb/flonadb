786b9f33d42156d1041648c289c703a1d1cf2ebe
wluyima <wyclif@openmrs.org>
2025-10-17 14:58:20 -0500

    Added server example

---
+++ examples/server/flona.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Add the JDBC drivers to this directory
+export LOADER_PATH=drivers
+export FLONA_FILE_DB_CFG_LOCATION=flona_db.properties
+MAIN_CLASS=com.amiyul.flona.db.remote.server.ServerBootstrap
+SPRING_LAUNCHER=org.springframework.boot.loader.PropertiesLauncher
+# You might have to change the server jar name below to match that of the downloaded file
+java -cp flona-server-1.2.0.jar -Dloader.main=$MAIN_CLASS $SPRING_LAUNCHER
